538752cde4a1f3dfdf2a2bcf75b2ad8279b553a9
MoPaMo <mpm1301@web.de>
2021-08-05 22:54:14 +0200

    forward / to index file

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+app.listen(port, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
