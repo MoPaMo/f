80664e5f8bee4b981ccc1d8e34600aae042b2728
MoPaMo <mpm1301@web.de>
2021-08-06 00:31:37 +0200

    fix console log

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ let db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READWRITE, (err) 
   if (err) {
     console.error(err.message);
   }
-  console.log('Connected to the chinook database.');
+  console.log('Connected to the database.');
 });
-
 
 
 app.get('/', function(req, res) {
