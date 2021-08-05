2c0c711274d1cd9b717240d26b221e5a95cf231c
MoPaMo <mpm1301@web.de>
2021-08-06 00:30:34 +0200

    use sqlite3

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = (process.env.PORT !=undefined ? process.env.PORT : 8080);
+const sqlite3 = require('sqlite3').verbose();
+
+// open the database
+let db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the chinook database.');
+});
+
 
 
 app.get('/', function(req, res) {
