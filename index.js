743ecc86ed7c7c3e487fd7790ead3503611e5118
MoPaMo <mpm1301@web.de>
2021-08-05 23:10:37 +0200

    static files unde /static

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = (process.env.PORT !=undefined ? process.env.PORT : 8080);
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
+app.use('/static', express.static(path.join(__dirname, '/static')));
 app.listen(port, function(err){
     if (err) console.log(err);
     console.log("Server listening on PORT", port);
