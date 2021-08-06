3d3ac5113f863560d9508617f12a9b1fc61899a2
MoPaMo <mpm1301@web.de>
2021-08-06 09:30:05 +0200

    fix false name in db connect

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = (process.env.PORT !=undefined ? process.env.PORT : 8080);
 const sqlite3 = require('sqlite3').verbose();
 
 // open the database
-let db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('./db/db.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
   }
